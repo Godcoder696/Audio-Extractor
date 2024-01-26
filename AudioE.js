7ab8efd281bd39dc36c5cf33b4d68a8dec91b218
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-01-26 15:49:17 +0530

    .env updated!

--- AudioE.js
+++ AudioE.js
@@ -1,14 +1,25 @@
 const extractAudio= require('ffmpeg-extract-audio');
 
-console.log("AUDIO EXTRACTOR!");
+console.log("WELCOME TO AUDIO EXTRACTOR!\n");
+
+const input= process.env.FILE_NAME ||  "sample_1280x720_surfing_with_audio.mp4" ;
+const output= process.env.OUTPUT_FILE_NAME || "output.mp3";
 
 async function audioExtracter(){
-    console.log("Processing");
-    const result=await extractAudio({
-        input: './mediaFiles/sample_1280x720_surfing_with_audio.mp4',
-        output:'./output/test.mp3'
-    })
-    console.log("Processing Done!");
+    try {
+
+        console.log("Converting File mp4 to mp3\n");
+
+        const result=await extractAudio({
+            input: `./mediaFiles/${input}`,
+            output: `./output/${output}`
+        })
+
+        console.log("Conversion Successfully Completed!\n Check output folder to locate the mp3 file");
+
+    } catch (error) {
+        console.log("Error Occurred!\n",error);
+    }
 }
 
 audioExtracter()
